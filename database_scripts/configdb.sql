a4960b6a11f393fa711fe6b7c02f417ff10b811d
OlhaPolishchuk <olapol6@gmail.com>
2016-12-12 21:46:13 +0200

    the new version of file

--- database_scripts/configdb.sql
+++ database_scripts/configdb.sql
@@ -1,9 +1,5 @@
 DROP DATABASE IF EXISTS journallpnu;
 CREATE DATABASE journallpnu;
 USE journallpnu;
-GRANT USAGE ON *.* TO 'rich'@'localhost' IDENTIFIED BY '<richrich1234>';
-GRANT ALL PRIVILEGES ON journallpnu.* TO 'rich'@'localhost';
-FLUSH PRIVILEGES;
-USE journallpnu;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, login VARCHAR(100), user_name VARCHAR(100), email VARCHAR(100), password VARCHAR(100));
