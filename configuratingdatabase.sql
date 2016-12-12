ebb59f9dad5df1645b754e4fb3acf036f99c2fab
OlhaPolishchuk <olapol6@gmail.com>
2016-12-12 21:28:06 +0200

    adding a new version of file

--- configuratingdatabase.sql
+++
@@ -1,6 +0,0 @@
-DROP DATABASE IF EXISTS journallpnu;
-CREATE DATABASE journallpnu;
-USE journallpnu;
-GRANT USAGE ON *.* TO 'root'@'localhost' IDENTIFIED BY '<richbeach>';
-GRANT ALL PRIVILEGES ON journallpnu.* TO 'root'@'localhost';
-FLUSH PRIVILEGES;
