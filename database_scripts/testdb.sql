bcf696a2529c6aa53a87ce718f1fc42bf4121846
OlhaPolishchuk <olapol6@gmail.com>
2016-12-17 12:01:53 +0200

    testdb

---
+++ database_scripts/testdb.sql
@@ -0,0 +1,3 @@
+USE journallpnu;
+INSERT INTO users VALUES (1, 'testinglogin', 'testingname', 'testingemail', 'testingpassword'), (2, 'testl', 'testn', 'teste', 'testp'), (3, 'one', 'two', 'three', 'four'),
+(4, 'login', 'name', 'email', 'password'), (5, 'logintest', 'nametest', 'emailtest', 'passwordtest')
