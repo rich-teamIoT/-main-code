94108387b31a872035c3f63d6ae566065d45ded1
Kamikadze9xdd <nazar.kulik99@gmail.com>
2016-12-22 21:03:59 +0200

    Change

--- Doctor_page.py
+++ Doctor_page.py
@@ -1,3 +1,3 @@
-from flask import Blueprint
+#from flask import Blueprint
 
-profesor_page=Blueprint('profesor_page', __name__)
+#profesor_page=Blueprint('profesor_page', __name__)
--- Student_page.py
+++ Student_page.py
@@ -1,20 +1,27 @@
 from Config import mysql
 from flask import Blueprint, request, abort, jsonify
 
-
-student_page=Blueprint('student_page', __name__)
+student_page=Blueprint('student_page',__name__)
 
 @student_page.route('/api/user_page_student', methods=['Put'])
-def student_page():
-    if not request.json or 'id' not in request:
-       abort(400)
+def STpage():
+    if not request.json or 'id' not in request.json:
+        abort(400)
     id=request.json['id']
     if id is None:
         abort(400)
-    conn=mysql.connect()
-    cur=conn.cursor()
-    query_id = 'select discipline_name from discipline where id=%s'
-    param_id = (id)
-    cur.execute(query_id, param_id)
-    data = cur.fetchone()
-    return jsonify(subject="%s" % data), 200
+
+    conn = mysql.connect()
+    cur = conn.cursor()
+    query = 'select exists(select * from discipline where id=%s )'
+    param = (id)
+    cur.execute(query, param)
+    if cur.fetchone()[0]==1:
+        conn=mysql.connect()
+        cur=conn.cursor()
+        query_id = 'select discipline_name from discipline where id=%s'
+        param_id = (id)
+        cur.execute(query_id, param_id)
+        data = cur.fetchone()
+        return jsonify(subject="%s" % data), 200
+    cur.close()
